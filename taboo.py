66c452a2ffebcbc745a3cd824350c31574a472b9
Aditya Pandey <apande9@illinois.edu>
2019-12-28 19:29:10 -0600

    add files

--- taboo.py
+++ taboo.py
@@ -1,72 +1,49 @@
 import praw
 import re
+import csv
 import operator
-import redis
 import os
-from prawcore.exceptions import NotFound
-
-def main(): # todo: refactor main()/login/exception code
-    try:
-        print("logging in...")
-        reddit = praw.Reddit(user_agent=os.environ.get('USER_AGENT'), 
-                             client_id=os.environ.get('CLIENT_ID'), client_secret=os.environ.get('CLIENT_SECRET'), 
-                             username=os.environ.get('USER_NAME'), password=os.environ.get('PASSWORD'))
-        print("listening...")
-        post = reddit.submission('aaaaaaaaaaa')
-        print(post.link_flair_text)
-        # subreddit = reddit.subreddit('hiphopcirclejerk+denzelcurry+test+freebies')
-        # global leaderboard
-        # leaderboard = reddit.submission(id='cfg95e')
-        # for comment in subreddit.stream.comments():
-        #     if comment.author and comment.author.name == 'cysvendson':
-        #           reddit.redditor(owner).message('sticker', comment.permalink)
-        #           print("PM'ed")
-        #           continue
-        #     if re.search("[!+]taboo", comment.body) or '!delete' in comment.body:
-        #         process_comment(comment)
-    except NotFound as e:
-        print("main(): exception occurred")
-        print(e)
-        print(type(e))
-        # main()
 
 def process_comment(comment):
     try:
         body = comment.body
         user = comment.author.name
         linkID = comment.link_id
-        cid = comment.id
+        id = comment.id
         parent = comment.parent()
 
-        if history.exists(cid) or user == 'nathancountbot':
-            print(cid, 'already parsed')
-            return
-
         # !delete
-        if (triggers[2] in body and isinstance(parent, commentType) and isinstance(parent.parent(), commentType) and parent.author.name == 'F1N1ZH_EM_ZEL' and (parent.parent().author.name == user or owner in user)):
+        if (triggers[2] in body and isinstance(parent, commentType) and isinstance(parent.parent(), commentType)
+            and parent.author.name == 'F1N1ZH_EM_ZEL' and (parent.parent().author.name == user or 'adityapstar' in user)):
                 grandparentUser = parent.parent().author.name
                 parentBody = parent.body
                 parentLinkID = parent.link_id
                 parentID = parent.id
                 parent.delete()
                 print('Deleted: "{}" by u/{} ({}, {})'.format(parentBody, grandparentUser, parentLinkID, parentID))
-                history.set(cid, user)
+                open("comments.txt", "a").write(id + '\n')
+                return
+
+        # skip if already parsed
+        with open('comments.txt', 'r') as file:
+            if id in file.read().splitlines():
                 return
 
         # keep track of number of times each user has called the bot
-        if user != owner:
-            points.incr(user)
-            if user in users:
-                users[user] += 1
-            else:
-                users[user] = 1
+        if user in users:
+            users[user] += 1
+        elif user != 'adityapstar':
+            users[user] = 1
+        with open('users.csv', 'w', newline='') as f:
+            w = csv.writer(f)
+            w.writerows(users.items())
         sortedUsers = sorted(users.items(), key=operator.itemgetter(1), reverse=True)
         nathans = 'u/ | bruh moments\n---|---\n🏆'
         for username, val in sortedUsers:
             if val > 1:
                 nathans += username.replace('_','\_') + ' | ' + str(val) + '\n'
-        if leaderboard.selftext + '\n' != nathans:
-            leaderboard.edit(nathans)
+        if submission.selftext+'\n' != nathans:
+            submission.edit(nathans)
 
         # determine which trigger
         indexOfOriginal = body.find(triggers[0])
@@ -101,28 +78,29 @@ def process_comment(comment):
         message = removeNPC(message)
 
         # discourage repeated calls
-        if (message == kanye):
+        if (message == "[HTTPZ://1.1MGUR.COM/CDTL4VX.G1F](https://i.imgur.com/cDTl4Vx.gif)"):
             print("replied to kanye.gif")
-            comment.reply(kanye)
-            history.set(cid, user)
+            comment.reply("[HTTPZ://1.1MGUR.COM/CDTL4VX.G1F](https://i.imgur.com/cDTl4Vx.gif)")
+            open("comments.txt", "a").write(id + '\n')
             return
 
         final = tabooify(message, trigger)
         if len(final) > 9999:
-            print("u/{}'s comment was too long ({}, {}, {})".format(user, linkID, cid, len(final)))
-            comment.reply(kanye)
-            history.set(cid, user)
+            print("u/{}'s comment was too long ({}, {}, {})".format(user, linkID, id, str(len(final))))
+            comment.reply("[HTTPZ://1.1MGUR.COM/CDTL4VX.G1F](https://i.imgur.com/cDTl4Vx.gif)")
+            open("comments.txt", "a").write(id + '\n')
             return
 
         # success
-        print('Replying to: "{}" by u/{} ({}, {})'.format(body, user, linkID, cid))
+        print('Replying to: "{}" by u/{} ({}, {})'.format(body, user, linkID, id))
         comment.reply(final)
-        history.set(cid, user)
+        with open('comments.txt', 'a') as file:
+            file.write(id + '\n')
     except Exception as e:
         print("process_comment(): exception occurred")
         print(e)
-        print('Tried replying to: "{}" by u/{} ({}, {})'.format(body, user, linkID, cid))
-        history.set(cid, user)
+        print('Tried replying to: "{}" by u/{} ({}, {})'.format(body, user, linkID, id))
+        open("comments.txt", "a").write(id + '\n')
         return
 
 def removeNPC(string):
@@ -135,7 +113,7 @@ def applySwaps(swaps, message):
     return final
 
 def tabooify(original, trigger):
-    original = original.upper().replace('NIGGER','NATHAN').replace('NIGGA','NATHAN')
+    original = original.upper()
     if (trigger == triggers[0]):
         swaps = {'B':'13','S':'Z','I':'1','#':'','13LACK METAL TERROR1ZT':'13 M T'}
     else:
@@ -146,12 +124,18 @@ def tabooify(original, trigger):
     return final
 
 if __name__ == '__main__':
-    # triggers = ['!taboo', '+taboo', '!delete']
-    # owner = os.environ.get('OWNER')
-    # commentType = praw.models.reddit.comment.Comment
-    # kanye = '[HTTPZ://1.1MGUR.COM/CDTL4VX.G1F](https://i.imgur.com/cDTl4Vx.gif)'
-    # history = redis.from_url(os.environ.get("REDIS_URL"), db=0)
-    # points = redis.from_url(os.environ.get("REDIS_URL"), db=1)
-    # print('loading users...')
-    # users = {user.decode():int(points.get(user).decode()) for user in points.scan_iter()}
-    main()
+    triggers = ['!taboo', '+taboo', '!delete']
+    commentType = praw.models.reddit.comment.Comment
+    with open('users.csv','r') as f:
+        reader = csv.reader(f)
+        users = {rows[0]:int(rows[1]) for rows in reader}
+    print("logging in...")
+    reddit = praw.Reddit(user_agent=os.environ.get('USER_AGENT'), 
+                             client_id=os.environ.get('CLIENT_ID'), client_secret=os.environ.get('CLIENT_SECRET'), 
+                             username=os.environ.get('USER_NAME'), password=os.environ.get('PASSWORD'))
+    print("listening...")
+    subreddit = reddit.subreddit('test')#'hiphopcirclejerk+denzelcurry+test+freebies')
+    submission = reddit.submission(id='cfg95e')
+    for comment in subreddit.stream.comments():
+        if re.search("[!+]taboo", comment.body) or '!delete' in comment.body:
+            process_comment(comment)
